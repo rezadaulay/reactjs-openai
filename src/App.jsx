6be598af8abbd36c246e3f10dc7909723759beb3
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2024-05-23 10:13:22 +0700

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={`container`}>
+        <h1 className={`app-title`}><span><img src="./favicon.png" className={`logo`} /></span>Virtual Travel Guide</h1>
+        <p className={`text-center`}>Hey there! Ready to plan your dream getaway? Just fill in the form below with your preferences, and we'll whip up some awesome vacation suggestions just for you!</p>
+        <form action="">
+          <div className={`form-group`}>
+              <label htmlFor="">Destination Preferences</label>
+              <input type="text" autoFocus />
+              <small>Choose your preferred destination type(s).</small>
+          </div>
+          <div className={`form-group`}>
+              <label htmlFor="">Travel Interests</label>
+              <input type="text" />
+              <small>Select your interests for the trip. Separate by commas.</small>
+          </div>
+          <div className={`form-group`}>
+              <label htmlFor="">Budget</label>
+              <input type="text" />
+              <small>Specify your trip budget range.</small>
+          </div>
+          <div className={`form-group`}>
+              <label htmlFor="">Travel Dates</label>
+              <input type="text" />
+              <small>Pick your start and end dates.</small>
+          </div>
+          <div className={`form-group`}>
+              <label htmlFor="">Period in Days</label>
+              <input type="text" />
+              <small>Enter the duration of your trip.</small>
+          </div>
+          <button>Submit</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
